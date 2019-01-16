84cc596b8182ea0be634f4e3c4b84a06b420cc30
Rafal Pijet <stronglopez@wp.pl>
2019-01-16 12:18:23 +0100

    Adding recommendation from Task 15.4

--- js/script.js
+++ js/script.js
@@ -17,5 +17,52 @@
     galaxyS6.printInfo();
     onePlus.printInfo();
 
+    Phone.prototype.printInfo = function () {
+        console.log("Change form of info --> " + this.price + ";" + this.brand + ";" + this.color);
+    }
 
+    console.log("\n");
+    iPhone6s.printInfo();
+    galaxyS6.printInfo();
+    onePlus.printInfo();
+
+    function Phone(brand, price, color, incomingMessages, outgoingMessages) {
+        this.brand = brand;
+        this.price = price;
+        this.color = color;
+        this.incomingMessages = incomingMessages;
+        this.outgoingMessages = outgoingMessages;
+    }
+
+    var inMessages = ["inFirst", "inSecond", "inThird", "inFourth", "inFifth"];
+    var outMessages = ["outFirst", "outSecond", "outThird", "outFourth", "outFifth"];
+    var iPhone7 = new Phone("Apple", 2800, "pink", inMessages, outMessages);
+    console.log("\n");
+    iPhone7.printInfo();
+
+    Phone.prototype.showIncomingMessages = function () {
+
+        for (var i = 0; i < this.incomingMessages.length; i++) {
+            console.log((i + 1) + ". " + this.incomingMessages[i] + ",");
+        }
+    }
+
+    Phone.prototype.showOutgoingMessages = function () {
+
+        for (var i = 0; i < this.outgoingMessages.length; i++) {
+            console.log((i + 1) + ". " + this.outgoingMessages[i] + ",");
+        }
+    }
+
+    Phone.prototype.howMuchCost = function (quantity) {
+        console.log("Price for " + quantity + " pieces: " + quantity * this.price);
+    }
+
+    console.log("\n");
+    iPhone7.showIncomingMessages();
+    console.log("\n");
+    iPhone7.showOutgoingMessages();
+    console.log("\n");
+    iPhone7.howMuchCost(2);
+    iPhone7.howMuchCost(7);
 })();
